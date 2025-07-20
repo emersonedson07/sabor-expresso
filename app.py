32940b84ea2ce115eea3fa6209c3de30e2b20c6c
emersonedson07 <emerson.edson07@gmail.com>
2025-07-20 16:00:27 -0300

    primeiro contato com python

---
+++ app.py
@@ -0,0 +1,48 @@
+import os
+
+def exibir_nome_do_programa():
+
+        print ('
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗░█████╗░
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝██╔══██╗
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░██║░░██║
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗██║░░██║
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝╚█████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░░╚════╝░\n')
+
+def exirbir_opcoes ():
+        print ('1. Cadastrar Restaurante')
+        print ('2. Listar Restaurante')
+        print ('3. Ativar Restaurante')
+        print ('4. Sair Restaurante\n')
+
+
+def finalizando_app ()
+os.system("cls")
+print("finalizando o app\n")
+
+def escolher_opcoes():
+        opcao_escolhida= int (input ("Escolha uma opção"))
+
+        if opcao_escolhida == 1:
+        print ('Cadastrar Restaurante')
+
+        elif opcao_escolhida == 2:
+        print ('Listar Restaurante')
+
+        elif opcao_escolhida == 3:              
+        print ('Ativar Restaurante')
+
+        else:
+         print ('Enerrando o programa')
+
+       finalizando_app ()
+
+def main()
+        exibir_nome_do_programa()
+        exirbir_opcoes()
+        escolher_opcoes()
+
+
+if __main__ =="__main__":
+        main()
